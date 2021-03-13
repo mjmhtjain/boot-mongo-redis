23b71826c5d75637ebb5f86e958ac464f69bf693
mohitjain@optum.com <mohitjain@optum.com>
2021-03-13 23:10:16 +0530

    dockerizing and setting K8 deployment settings

--- azure-setup-postgres.sh
+++ azure-setup-postgres.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 set -e
 
-AZ_RESOURCE_GROUP=webfluxapiservice
+AZ_RESOURCE_GROUP=simpledbresourcegroup
 AZ_LOCATION=eastus
-AKS_POSTGRES=webfluxpostgres
-AKS_POSTGRES_USERNAME=webfluxadmin
-AKS_POSTGRES_PASSWORD=webfluxadmin
+
+AKS_POSTGRES=simpledbpostgres
+AKS_POSTGRES_USERNAME=postgresadmin
+AKS_POSTGRES_PASSWORD=postgresadmin
 IP_ADDR=$(curl "http://whatismyip.akamai.com/")
+
+AZ_ACR=simpledbregistry
+ACR_LOGIN_SERVER=simpledbregistryserver
+AZ_ACR_PASSWORD=password
+
+DOCKER_IMAGE_TAG=simpledb
+
+AZ_AKS=simpledbakscluster
+AZ_DNS_PREFIX=simpledbkubernetes
+AKS_POD=simpledbpod
+
+# Create a resource group.
+az group create \
+    --name $AZ_RESOURCE_GROUP \
+    --location $AZ_LOCATION \
+    | jq
 
 # Create postgres flex server
 az postgres flexible-server create \
@@ -16,3 +33,56 @@ az postgres flexible-server create \
   --admin-user $AKS_POSTGRES_USERNAME \
   --admin-password $AKS_POSTGRES_PASSWORD \
   --public-access $IP_ADDR
+
+# Create a registry
+az acr create --resource-group $AZ_RESOURCE_GROUP \
+  --location $AZ_LOCATION \
+  --name $AZ_ACR \
+  --sku Basic \
+  --admin-enabled true
+
+ACR_LOGIN_SERVER=$(az acr list \
+  --resource-group $AZ_RESOURCE_GROUP \
+  | jq -r '.[0].loginServer')
+
+
+
+# login to registry
+AZ_ACR_PASSWORD=$(az acr credential show \
+  --resource-group $AZ_RESOURCE_GROUP \
+  --name $AZ_ACR | jq -r '.passwords[0].value')
+
+# build docker image
+docker build --tag $DOCKER_IMAGE_TAG .
+docker tag $DOCKER_IMAGE_TAG $ACR_LOGIN_SERVER/$DOCKER_IMAGE_TAG:latest
+
+# push image to ACR
+echo $AZ_ACR_PASSWORD | docker login $ACR_LOGIN_SERVER \
+  --username $AZ_ACR \
+  --password-stdin
+
+docker push $ACR_LOGIN_SERVER/$DOCKER_IMAGE_TAG:latest
+
+
+# create AKS cluster
+az aks create \
+  --resource-group=$AZ_RESOURCE_GROUP \
+  --name=$AZ_AKS \
+  --vm-set-type VirtualMachineScaleSets \
+  --enable-cluster-autoscaler \
+  --min-count 3 \
+  --max-count 5 \
+  --attach-acr $AZ_ACR \
+  --dns-name-prefix=$AZ_DNS_PREFIX \
+  --generate-ssh-keys \
+  --load-balancer-sku standard
+
+az aks get-credentials \
+  --resource-group=$AZ_RESOURCE_GROUP \
+  --name=$AZ_AKS \
+  --overwrite-existing
+
+# deploy image to AKS instance
+kubectl apply -f deployment.yaml
+
+kubectl get services --watch
