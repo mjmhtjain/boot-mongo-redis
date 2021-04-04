2fba583f4c0cd3541d6ca24b10940775b0af5e75
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2021-04-04 15:16:53 +0530

    pushing this to new repo

--- azure-setup-postgres.sh
+++ azure-setup-postgres.sh
@@ -8,19 +8,6 @@ COSMOS_ACCOUNT_NAME="cosmos-13767"
 DATABASE_NAME='database1'
 COLLECTION_NAME='ShoppingCart'
 
-KUBE_RESOURCE_GROUP=simpledbrg
-KUBE_LOCATION=eastus
-
-KUBE_ACR=simpledbregistry
-ACR_LOGIN_SERVER=simpledbregistryserver
-AZ_ACR_PASSWORD=password
-
-DOCKER_IMAGE_TAG=simpledb:v1.2
-
-AZ_AKS=simpledbakscluster
-AZ_DNS_PREFIX=simpledbkubernetes
-AKS_POD=simpledbpod
-
 # Setting up Mongo Resources
 az group create \
     --name $COSMOS_RESOURCE_GROUP \
@@ -46,7 +33,29 @@ az cosmosdb mongodb collection create \
     --throughput "10000"
 
 
+# Redis Cluster setup
+REDIS_RESOURCE_GROUP=redisrg
+REDIS_LOCATION=eastus
+REDIS_NAME=redis-13767
+
+az redis create \
+  --location $REDIS_LOCATION \
+  --name $REDIS_NAME \
+  --resource-group $REDIS_RESOURCE_GROUP \
+  --sku Basic \
+  --minimum-tls-version 1.2
+  --vm-size c0 ## {c0, c1, c2, c3, c4, c5, c6, p1, p2, p3, p4, p5}
+
 # Setup K8 cluster and registry
+KUBE_RESOURCE_GROUP=simpledbrg
+KUBE_LOCATION=eastus
+
+KUBE_ACR=simpledbregistry
+DOCKER_IMAGE_TAG=simpledb:v1.2
+
+KUBE_AKS=simpledbakscluster
+KUBE_DNS_PREFIX=simpledbkubernetes
+AKS_POD=simpledbpod
 
 az group create \
     --name $KUBE_RESOURCE_GROUP \
@@ -69,19 +78,19 @@ az acr import  \
 # create AKS cluster
 az aks create \
   --resource-group $KUBE_RESOURCE_GROUP \
-  --name $AZ_AKS \
+  --name $KUBE_AKS \
   --vm-set-type VirtualMachineScaleSets \
   --enable-cluster-autoscaler \
   --min-count 3 \
   --max-count 5 \
   --attach-acr $KUBE_ACR \
-  --dns-name-prefix $AZ_DNS_PREFIX \
+  --dns-name-prefix $KUBE_DNS_PREFIX \
   --generate-ssh-keys \
   --load-balancer-sku standard
 
 az aks get-credentials \
   --resource-group=$KUBE_RESOURCE_GROUP \
-  --name=$AZ_AKS \
+  --name=$KUBE_AKS \
   --overwrite-existing
 
 # deploy image to AKS instance
