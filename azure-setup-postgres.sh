c2f5969ca6ebaddf4e386aebbad32ee9d798ea9a
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2021-04-04 10:07:37 +0530

    Deployment Scaling fails traffic for a split second

--- azure-setup-postgres.sh
+++ azure-setup-postgres.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
-uniqueId=$RANDOM
-AZ_RESOURCE_GROUP=cosmosdbrg
-AZ_LOCATION=eastus
+COSMOS_RESOURCE_GROUP=cosmosdbrg
+COSMOS_LOCATION=eastus
 
-accountName="cosmos-$uniqueId"
-databaseName='database1'
-collectionName='ShoppingCart'
+COSMOS_ACCOUNT_NAME="cosmos-13767"
+DATABASE_NAME='database1'
+COLLECTION_NAME='ShoppingCart'
 
-AZ_ACR=simpledbregistry
+KUBE_RESOURCE_GROUP=simpledbrg
+KUBE_LOCATION=eastus
+
+KUBE_ACR=simpledbregistry
 ACR_LOGIN_SERVER=simpledbregistryserver
 AZ_ACR_PASSWORD=password
 
@@ -19,61 +21,66 @@ AZ_AKS=simpledbakscluster
 AZ_DNS_PREFIX=simpledbkubernetes
 AKS_POD=simpledbpod
 
-# Create a resource group.
+# Setting up Mongo Resources
 az group create \
-    --name $AZ_RESOURCE_GROUP \
-    --location $AZ_LOCATION \
+    --name $COSMOS_RESOURCE_GROUP \
+    --location $COSMOS_LOCATION \
     | jq
 
-# Create MongoDB server
 az cosmosdb create \
-    -n $accountName \
-    -g $AZ_RESOURCE_GROUP \
+    -n $COSMOS_ACCOUNT_NAME \
+    -g $COSMOS_RESOURCE_GROUP \
     --kind MongoDB \
     --locations regionName='East US' failoverPriority=0 isZoneRedundant=True
 
 az cosmosdb mongodb database create \
-    -a $accountName \
-    -g $AZ_RESOURCE_GROUP \
-    -n $databaseName
+    -a $COSMOS_ACCOUNT_NAME \
+    -g $COSMOS_RESOURCE_GROUP \
+    -n $DATABASE_NAME
 
 az cosmosdb mongodb collection create \
-    -a $accountName \
-    -g $AZ_RESOURCE_GROUP \
-    -d $databaseName \
-    -n $collectionName \
+    -a $COSMOS_ACCOUNT_NAME \
+    -g $COSMOS_RESOURCE_GROUP \
+    -d $DATABASE_NAME \
+    -n $COLLECTION_NAME \
     --throughput "10000"
 
-# disable georedundancy and enable auto-scale in collection
+
+# Setup K8 cluster and registry
+
+az group create \
+    --name $KUBE_RESOURCE_GROUP \
+    --location $KUBE_LOCATION \
+    | jq
 
 # Create a registry
-az acr create --resource-group $AZ_RESOURCE_GROUP \
-  --location $AZ_LOCATION \
-  --name $AZ_ACR \
+az acr create --resource-group $KUBE_RESOURCE_GROUP \
+  --location $KUBE_LOCATION \
+  --name $KUBE_ACR \
   --sku Basic \
   --admin-enabled true
 
 # push image to ACR
 az acr import  \
-  -n $AZ_ACR \
+  -n $KUBE_ACR \
   --source docker.io/mjmhtjain/$DOCKER_IMAGE_TAG \
   --image $DOCKER_IMAGE_TAG
 
 # create AKS cluster
 az aks create \
-  --resource-group $AZ_RESOURCE_GROUP \
+  --resource-group $KUBE_RESOURCE_GROUP \
   --name $AZ_AKS \
   --vm-set-type VirtualMachineScaleSets \
   --enable-cluster-autoscaler \
   --min-count 3 \
   --max-count 5 \
-  --attach-acr $AZ_ACR \
+  --attach-acr $KUBE_ACR \
   --dns-name-prefix $AZ_DNS_PREFIX \
   --generate-ssh-keys \
   --load-balancer-sku standard
 
 az aks get-credentials \
-  --resource-group=$AZ_RESOURCE_GROUP \
+  --resource-group=$KUBE_RESOURCE_GROUP \
   --name=$AZ_AKS \
   --overwrite-existing
 
